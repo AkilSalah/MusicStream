cd8f5f47cb0a68559565275fd1613d55443e01bf
salah <akilsalaheddine203@gmail.com>
2025-01-01 15:21:20 +0100

    add library and track service,models and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'library', loadChildren: () => import('./features/library/library.module').then(m => m.LibraryModule) },
+   { path: 'track', loadChildren: () => import('./features/track/track.module').then(m => m.TrackModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
