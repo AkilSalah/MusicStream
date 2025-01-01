41220b96f53a67874d583467cf574c5f97e93cd0
salah <akilsalaheddine203@gmail.com>
2025-01-02 00:40:54 +0100

    library interface

--- src/app/core/models/track.ts
+++ src/app/core/models/track.ts
@@ -1,2 +1,18 @@
-export class Track {
+
+export enum MusicCategory {
+    POP = 'pop',
+    ROCK = 'rock',
+    RAP = 'rap',
+    CHAABI = 'cha3bi',
+    RAI = 'rai'
+  }
+
+export interface Track {
+  id: string;
+  title: string;
+  artist: string;
+  description?: string;
+  date: Date;
+  duration: number;
+  category: MusicCategory;
 }
