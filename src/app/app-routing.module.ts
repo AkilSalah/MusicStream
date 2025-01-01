42aa5aff6d15f86ff48d1245ce668dc6c03f7752
salah <akilsalaheddine203@gmail.com>
2025-01-01 15:24:55 +0100

    add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: 'library', loadChildren: () => import('./features/library/library.module').then(m => m.LibraryModule) },
-   { path: 'track', loadChildren: () => import('./features/track/track.module').then(m => m.TrackModule) }];
+   { path: 'track', loadChildren: () => import('./features/track/track.module').then(m => m.TrackModule) },
+   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
