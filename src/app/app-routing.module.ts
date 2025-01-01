e8f3e66e8c3fa979aaba115761deab606ffd7f44
salah <akilsalaheddine203@gmail.com>
2025-01-01 16:48:52 +0100

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'library', loadChildren: () => import('./features/library/library.module').then(m => m.LibraryModule) },
+const routes: Routes = [
+   { path: 'library', loadChildren: () => import('./features/library/library.module').then(m => m.LibraryModule) },
    { path: 'track', loadChildren: () => import('./features/track/track.module').then(m => m.TrackModule) },
-   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }];
+   { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
